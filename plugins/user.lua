82a24d335d156fa20d9c41f94a244eeeeaba9b83
dev-freelance-ru <dev.freelance@yandex.ru>
2024-01-07 21:52:06 +0300

    ...

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,25 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  --
+  -- cmd: the list of commands will be extended with your custom commands
+  -- event: the list of events will be extended with your custom events
+  -- ft: the list of filetypes will be extended with your custom filetypes
+  -- keys: the list of keymaps will be extended with your custom keymaps
+  -- opts: your custom opts will be merged with the default opts
+  -- dependencies: the list of dependencies will be extended with your custom dependencies
+  -- any other property will override the defaults
+  --
+  "olexsmir/gopher.nvim",
+  ft = "go",
+  config = function (_, opts)
+    require("gopher").setup(opts)
+  end,
+  build = function ()
+    vim.cmd [[silent! GoInstallDeps]]
+  end,
+  requires = { -- dependencies
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
 }
